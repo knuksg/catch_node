8cd799811aa11833be9b7cf505b36692f4de1563
knuksg <knuksg@naver.com>
2024-08-19 18:18:08 +0900

    auth

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,9 @@
-// const express = require("express");
-// const router = express.Router();
-// const { sendMessage, saveChatInfo, getChatInfo } = require("../controllers/chatController");
+const express = require("express");
+const router = express.Router();
+const { sendMessage, saveChatInfo, getChatInfo } = require("../controllers/chatController");
 
-// router.post("/", sendMessage);
-// router.post("/threadId", saveChatInfo);
-// router.get("/threadId", getChatInfo);
+router.post("/", sendMessage);
+router.post("/threadId", saveChatInfo);
+router.get("/threadId", getChatInfo);
 
-// module.exports = router;
+module.exports = router;
