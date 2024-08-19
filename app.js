e6119b3d7d1659d0a5826e9272a3fc3b03032f63
knuksg <knuksg@naver.com>
2024-08-19 12:45:38 +0900

    auth

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth"); // 새로 추가된 라우트
 
 var app = express();
+
+// CORS
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
